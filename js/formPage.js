ac1ab600b51742bf738c1fee5c077c21db2d4c64
charlotteswebdev <charlottevmcknight@gmail.com>
2025-08-04 18:04:20 +0100

    first commit

---
+++ js/formPage.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+    $('.month-checkbox').change(function() {
+        updateSelectedMonths();
+    });
+
+    function updateSelectedMonths() {
+        var selectedMonths = [];
+        
+        $('.month-checkbox:checked').each(function() {
+            selectedMonths.push($(this).val());
+        });
+
+        $('#selectedMonths').empty(); 
+        selectedMonths.forEach(function(month) {
+            $('#selectedMonths').append(`
+                <div class="selected-month" data-month="${month}">
+                    ${month} <span class="remove-month">&times;</span>
+                </div>
+            `);
+        });
+
+        $('.remove-month').click(function() {
+            var monthToRemove = $(this).parent().data('month');
+            $(`#${monthToRemove.toLowerCase()}`).prop('checked', false);  
+            updateSelectedMonths(); 
+        });
+    }
+
+    $('#joinForm').submit(function(event) {
+        event.preventDefault();
+
+        var firstName = $('#firstName').val();
+        var surname = $('#surname').val();
+        var email = $('#email').val();
+        var availableMonths = [];
+
+        $('.month-checkbox:checked').each(function() {
+            availableMonths.push($(this).val());
+        });
+
+        if (!firstName || !surname || !email) {
+            $('#formMessage').html('Please fill in all required fields.').css('color', 'red');
+            return;
+        }
+
+        if (availableMonths.length === 0) {
+            $('#formMessage').html('Please select at least one month you are available.').css('color', 'red');
+            return;
+        }
+    });
+});
